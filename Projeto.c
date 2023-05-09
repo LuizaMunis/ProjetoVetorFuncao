6a2d2febf2c611fa3d20a1608c34311f5aff5ab0
LuizaMunis <113556889+LuizaMunis@users.noreply.github.com>
2023-05-09 19:02:42 -0300

    Update Projeto.c

--- Projeto.c
+++ Projeto.c
@@ -1,28 +1,83 @@
 #include<stdio.h>
-#include <string.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
 
-int main (){
-    int  i=0;
-    char escolha,opcao[3];
+int numerosrandomicos(int  *id){
+    srand(time(NULL)); 
+    id = 1 + rand() % 1000;
+    printf("Id:%d", id);
+}
 
+char preenchernome(char *nome){
+    printf("\nDigite o nome completo: ");
+    fgets(nome,50,stdin);
+}
+
+char preencheremail(char *email){
+    int verificar;
+       do {
+        printf("Digite seu email: ");
+        fgets(email, 100, stdin);
+
+        if (strstr(email, "@") == NULL) {
+            printf("Inválido. Coloque o email com @.\n");
+        } else if (strstr(email, ".com") == NULL && strstr(email, ".br") == NULL) {
+            printf("Inválido. O email tem que terminar com (.com ou .br)\n");
+        } else {
+            verificar = 1;
+        }
+    } while (!verificar); 
+}
+
+//CORRIGIR
+
+char preenchersexo(char *sexo){
     do{
-     printf("Qual funcao deseja realizar?\n Digite i para incluir\n Digite d para deletar.\n Digite e para editar.\n");
-     scanf("%c", &escolha);
-     fflush (stdin);
+    printf("Digite o sexo.Caso nao queira identificar, digite indiferente: ");
+    fgets (sexo,11,stdin);
 
-     printf("Tem certeza?");
-     gets(opcao);
+    }while(strcmp(sexo, "feminino")!=0 || strcmp(sexo, "masculino")!=0 || strcmp(sexo, "indiferente")!=0);
+}
 
-        if(escolha!= 'i' || escolha!= 'e' || escolha!= 'd'){
-        printf("Opcao invalida\n");}
+int main() {
+    int i = 0;
+    int id[5];
+    char escolha, opcao[3], nome[50][5],email[50][5], sexo[11][5];
 
-    }while(strcmp(opcao, "nao")==0 || escolha!= 'i' || escolha!= 'e' || escolha!= 'd' && i<5);
+    do {
+        printf("Qual funcao deseja realizar?\nDigite i para incluir\nDigite d para deletar.\nDigite e para editar.\n");
+        scanf(" %c", &escolha);
+        fflush(stdin);
 
-    switch (escolha){
-     case 'i':break;
-     case 'd':break;
-     case 'e':break;
-     default:
-        break;
+        if (escolha != 'i' && escolha != 'e' && escolha != 'd') {
+            printf("Opcao invalida\n");
+        }
+
+        
+    switch (escolha) {
+        case 'i':
+         printf("\nFuncao incluir selecionada.\n");
+         numerosrandomicos(*id);
+         preenchernome(*nome);
+         preencheremail(*email);
+            break;
+
+        case 'd':
+            printf("Funcao deletar selecionada.\n");
+            break;
+
+        case 'e':
+            printf("Funcao editar selecionada.\n");
+            break;
+
+        default:
+            break;
     }
+       printf("\nDeseja continuar?");
+       gets(opcao);
+
+        i++;
+    } while ((escolha != 'i' && escolha != 'e' && escolha != 'd') && i < 5 && strcmp(opcao, "sim")==0);
+
 }
